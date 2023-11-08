afd44fd8056230556e3429db87ea32a5a421a0a9
Aaron Corns <aaron@sev7n.dev>
2023-11-07 22:23:02 -0500

    Basic Home, Register, Initial Database Model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User, AbstractUser
+from django.utils import timezone
+
+
+# Recipes Model for DB
+class Recipes(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
