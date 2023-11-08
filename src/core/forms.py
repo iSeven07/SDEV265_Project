d83256edffe71a34a505be0aa89393d528903367
justjoe19 <joeholl1979@icloud.com>
2023-11-08 00:09:33 -0500

    Upd readme file, Register and Submit Recipe pages

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Recipes
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
@@ -8,3 +9,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipes
+        fields = ['title', 'content']  # These are the fields you want to include in the form
