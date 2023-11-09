c9f18ed29fc515dbc8b263c1c508c7fb4595d56d
Aaron Corns <aaron@sev7n.dev>
2023-11-09 18:15:20 -0500

    Added login page and .gitignore file

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('search/', views.search, name="recipe-search"),
     path('register/', views.register, name="recipe-register"),
     path('submit_recipe/', views.submit_recipe, name='recipe-submit'),
+    path('login/', views.login_page, name='login'),
 ]
