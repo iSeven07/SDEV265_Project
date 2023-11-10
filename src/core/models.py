1c4f2c384febf8e60410117ede7f85ae653a706d
Aaron Corns <aaron@sev7n.dev>
2023-11-10 14:54:30 -0500

    Rem </hr> from base, dis img resizing in models

--- src/core/models.py
+++ src/core/models.py
@@ -24,16 +24,16 @@ class Profile(models.Model):
     avatar = models.ImageField(default='default.png', upload_to='profile_images')
     bio = models.TextField()
 
-    # resizing images
-    def save(self, *args, **kwargs):
-        super().save()
+    # # resizing images
+    # def save(self, *args, **kwargs):
+    #     super().save()
 
-        img = Image.open(self.avatar.path)
+    #     img = Image.open(self.avatar.path)
 
-        if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
-            img.save(self.avatar.path)
+    #     if img.height > 100 or img.width > 100:
+    #         new_img = (100, 100)
+    #         img.thumbnail(new_img)
+    #         img.save(self.avatar.path)
 
     def __str__(self):
         return self.user.username
