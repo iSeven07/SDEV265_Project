81fec065a8bcbcea792bf5bb4bc083de0d93ff69
Aaron Corns <aaron@sev7n.dev>
2023-12-08 15:01:00 -0500

    Update Model Name, Start of Ingredient

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Recipes, Profile
+from .models import Recipe, Profile, RecipeIngredient, Ingredient
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
@@ -18,11 +19,42 @@ class RecipeForm(forms.ModelForm):
         model = Recipes
         fields = ['title', 'content', 'ingredients']
 """
+# class IngredientForm(forms.ModelForm):
+#     ingredient = forms.CharField(max_length=63)
+#     quantity = forms.DecimalField(required=True)
+
+#     class Meta:
+#         model = RecipeIngredient
+#         fields = ['ingredient', 'quantity']
+#         widgets = {
+#             'ingredient': forms.TextInput(attrs={'placeholder': 'Enter ingredient name'}),
+#         }
+
+# IngredientFormSet = inlineformset_factory(Recipes, RecipeIngredient, form=IngredientForm, extra=1)
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ['ingredient', 'quantity']
+        widgets = {
+            'ingredient': forms.TextInput(attrs={'placeholder': 'Enter ingredient name'}),
+        }
+
+    ingredient = forms.CharField(max_length=100, required=False)
+
+    def clean_ingredient(self):
+        ingredient_name = self.cleaned_data['ingredient']
+        if not ingredient_name:
+            return None  # Handle the case when the field is empty
+        ingredient, created = Ingredient.objects.get_or_create(name=ingredient_name, defaults={'calories': 0})
+        return ingredient
+
+IngredientFormSet = inlineformset_factory(Recipe, RecipeIngredient, form=IngredientForm, extra=1)
 
 class RecipeForm(forms.ModelForm):
 
     class Meta:
-        model = Recipes
+        model = Recipe
         fields = ['title', 'content']  # These are the fields you want to include in the form
 
 class LoginForm(forms.Form):
